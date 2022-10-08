ee3a10f14cfbfd0a64052b5501602e6ec374d683
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-10-08 22:05:02 +0600

    some chanegs

---
+++ src/layout/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+        <Header></Header>
+        
+        </div>
+    );
+};
+
+export default Main;
